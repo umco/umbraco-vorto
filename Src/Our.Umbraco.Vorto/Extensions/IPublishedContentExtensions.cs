c3afa58ee6e59c1dedcbf10bf7a9f18da8941417
mattbrailsford <me@mattbrailsford.com>
2015-11-28 18:02:44 +0000

    Added support for supplying just a language as cultureName and vorto will find best match Changed recursive lookup to check individual languages rather than just checking an overall vorto value

--- Src/Our.Umbraco.Vorto/Extensions/IPublishedContentExtensions.cs
+++ Src/Our.Umbraco.Vorto/Extensions/IPublishedContentExtensions.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading;
-using Newtonsoft.Json;
 using Our.Umbraco.Vorto.Helpers;
 using Our.Umbraco.Vorto.Models;
 using Umbraco.Core;
@@ -16,6 +14,28 @@ namespace Our.Umbraco.Vorto.Extensions
 	{
 		#region HasValue
 
+        private static bool DoInnerHasVortoValue(this IPublishedContent content, string propertyAlias,
+            string cultureName = null, bool recursive = false)
+	    {
+            if (content.HasValue(propertyAlias))
+            {
+                var prop = content.GetProperty(propertyAlias);
+                var vortoModel = prop.Value as VortoValue;
+                if (vortoModel != null)
+                {
+                    var bestMatchCultureName = vortoModel.FindBestMatchCulture(cultureName);
+                    if (!bestMatchCultureName.IsNullOrWhiteSpace() 
+                        && vortoModel.Values[bestMatchCultureName] != null
+                        && !vortoModel.Values[bestMatchCultureName].ToString().IsNullOrWhiteSpace()) 
+                        return true;
+                }
+            }
+
+            return recursive && content.Parent != null 
+                ? content.Parent.DoInnerHasVortoValue(propertyAlias, cultureName, recursive)
+                : false;
+	    }
+
 		private static bool DoHasVortoValue(this IPublishedContent content, string propertyAlias,
             string cultureName = null, bool recursive = false)
 		{
@@ -25,20 +45,12 @@ namespace Our.Umbraco.Vorto.Extensions
 			if (!content.HasValue(propertyAlias, recursive))
 				return false;
 
-			var prop = content.GetProperty(propertyAlias, recursive);
-			if (prop.Value is VortoValue)
-			{
-				var vortoModel = prop.Value as VortoValue;
-				if (!vortoModel.Values.ContainsKey(cultureName) || vortoModel.Values[cultureName] == null
-					|| vortoModel.Values[cultureName].ToString().IsNullOrWhiteSpace())
-						return false;
-			}
-
-			return true;
+		    return content.DoInnerHasVortoValue(propertyAlias, cultureName, recursive);
 		}
 
         public static bool HasVortoValue(this IPublishedContent content, string propertyAlias,
-            string cultureName = null, bool recursive = false, string fallbackCultureName = null)
+            string cultureName = null, bool recursive = false, 
+            string fallbackCultureName = null)
         {
             var hasValue = content.DoHasVortoValue(propertyAlias, cultureName, recursive);
             if (!hasValue && !string.IsNullOrEmpty(fallbackCultureName) && !fallbackCultureName.Equals(cultureName))
@@ -48,156 +60,110 @@ namespace Our.Umbraco.Vorto.Extensions
 
 		#endregion
 
-		#region IfHasValue
-
-		// String
-		//public static IHtmlString IfHasVortoValue(this IPublishedContent content, string propertyAlias, 
-		//	string valueIfTrue, string valueIfFalse = null)
-		//{
-		//	return !content.HasVortoValue(propertyAlias) 
-		//		? new HtmlString(valueIfFalse ?? string.Empty) 
-		//		: new HtmlString(valueIfTrue);
-		//}
-
-		// No type
-		//public static HelperResult IfHasVortoValue(this IPublishedContent content, string propertyAlias,
-		//	Func<object, HelperResult> templateIfTrue, Func<object, HelperResult> templateIfFalse = null)
-		//{
-		//	return content.IfHasVortoValue()
-		//}
-
-		// Type
-		//public static HelperResult IfHasVortoValue<T>(this IPublishedContent content, string propertyAlias,
-		//	Func<T, HelperResult> templateIfTrue, Func<T, HelperResult> templateIfFalse = null)
-		//{
-		//	return new HelperResult(writer =>
-		//	{
-		//		if (!content.HasVortoValue(propertyAlias))
-		//		{
-		//			if (templateIfFalse != null)
-		//				templateIfFalse(null).WriteTo(writer);
-		//		}
-		//		else
-		//		{
-		//			var value = content.GetVortoValue(propertyAlias);
-		//			templateIfTrue(value).WriteTo(writer);
-		//		}
-		//	});
-		//}
-
-		#endregion
-
 		#region GetValue
 
-		private static object DoGetVortoValue(this IPublishedContent content, string propertyAlias, string cultureName = null,
-			bool recursive = false, object defaultValue = null)
-		{
-			return content.GetVortoValue<object>(propertyAlias, cultureName, recursive, defaultValue);
-		}
-
-        public static object GetVortoValue(this IPublishedContent content, string propertyAlias, string cultureName = null,
-            bool recursive = false, object defaultValue = null, string fallbackCultureName = null)
+        private static T DoInnerGetVortoValue<T>(this IPublishedContent content, string propertyAlias, string cultureName = null,
+            bool recursive = false, T defaultValue = default(T))
         {
-            var result = content.DoGetVortoValue(propertyAlias, cultureName, recursive);
-            if (result == null && !string.IsNullOrEmpty(fallbackCultureName) && !fallbackCultureName.Equals(cultureName))
-                result = content.DoGetVortoValue(propertyAlias, fallbackCultureName, recursive, defaultValue);
+            if (content.HasValue(propertyAlias))
+            {
+                var prop = content.GetProperty(propertyAlias);
+                var vortoModel = prop.Value as VortoValue;
+                if (vortoModel != null)
+                {
+                    // Get the serialized value
+                    var bestMatchCultureName = vortoModel.FindBestMatchCulture(cultureName);
+                    var value = vortoModel.Values[bestMatchCultureName];
 
-            return result;
+                    if (value != null && !value.ToString().IsNullOrWhiteSpace())
+                    {
+                        // Get target datatype
+                        var targetDataType = VortoHelper.GetTargetDataTypeDefinition(vortoModel.DtdGuid);
+
+                        // Umbraco has the concept of a IPropertyEditorValueConverter which it 
+                        // also queries for property resolvers. However I'm not sure what these
+                        // are for, nor can I find any implementations in core, so am currently
+                        // just ignoring these when looking up converters.
+                        // NB: IPropertyEditorValueConverter not to be confused with
+                        // IPropertyValueConverter which are the ones most people are creating
+                        var properyType = CreateDummyPropertyType(targetDataType.Id, targetDataType.PropertyEditorAlias, content.ContentType);
+                        var converters = PropertyValueConvertersResolver.Current.Converters.ToArray();
+
+                        // In umbraco, there are default value converters that try to convert the 
+                        // value if all else fails. The problem is, they are also in the list of
+                        // converters, and the means for filtering these out is internal, so
+                        // we currently have to try ALL converters to see if they can convert
+                        // rather than just finding the most appropreate. If the ability to filter
+                        // out default value converters becomes public, the following logic could
+                        // and probably should be changed.
+                        foreach (var converter in converters.Where(x => x.IsConverter(properyType)))
+                        {
+                            // Convert the type using a found value converter
+                            var value2 = converter.ConvertDataToSource(properyType, value, false);
+
+                            // If the value is of type T, just return it
+                            if (value2 is T)
+                                return (T)value2;
+
+                            // Value is not final value type, so try a regular type conversion aswell
+                            var convertAttempt = value2.TryConvertTo<T>();
+                            if (convertAttempt.Success)
+                                return convertAttempt.Result;
+
+                            // If ConvertDataToSource failed try ConvertSourceToObject.
+                            var value3 = converter.ConvertSourceToObject(properyType, value, false);
+
+                            // If the value is of type T, just return it
+                            if (value3 is T)
+                                return (T)value3;
+
+                            // Value is not final value type, so try a regular type conversion aswell
+                            var convertAttempt2 = value3.TryConvertTo<T>();
+                            if (convertAttempt2.Success)
+                                return convertAttempt2.Result;
+                        }
+
+                        // Value is not final value type, so try a regular type conversion
+                        var convertAttempt3 = value.TryConvertTo<T>();
+                        if (convertAttempt3.Success)
+                            return convertAttempt3.Result;
+
+                        // Still not right type so return default value
+                        return defaultValue;
+
+                    }
+                }
+            }
+
+            return recursive && content.Parent != null
+                ? content.Parent.DoInnerGetVortoValue<T>(propertyAlias, cultureName, recursive, defaultValue)
+                : defaultValue;
         }
 
-		private static T DoGetVortoValue<T>(this IPublishedContent content, string propertyAlias, string cultureName = null, 
+		private static T DoGetVortoValue<T>(this IPublishedContent content, string propertyAlias, string cultureName = null,
             bool recursive = false, T defaultValue = default(T))
 		{
 			if (cultureName == null)
 				cultureName = Thread.CurrentThread.CurrentUICulture.Name;
 
-			if (content.HasVortoValue(propertyAlias, cultureName, recursive))
-			{
-				var prop = content.GetProperty(propertyAlias, recursive);
-				if (prop.Value is VortoValue)
-				{
-					// Get the serialized value
-					var vortoModel = prop.Value as VortoValue;
-					var value = vortoModel.Values[cultureName];
-
-					// If the value is of type T, just return it
-					//if (value is T)
-					//	return (T)value;
-
-					// Get target datatype
-					var targetDataType = VortoHelper.GetTargetDataTypeDefinition(vortoModel.DtdGuid);
-
-					// Umbraco has the concept of a IPropertyEditorValueConverter which it 
-					// also queries for property resolvers. However I'm not sure what these
-					// are for, nor can I find any implementations in core, so am currently
-					// just ignoring these when looking up converters.
-					// NB: IPropertyEditorValueConverter not to be confused with
-					// IPropertyValueConverter which are the ones most people are creating
-                    var properyType = CreateDummyPropertyType(targetDataType.Id, targetDataType.PropertyEditorAlias, content.ContentType);
-					var converters = PropertyValueConvertersResolver.Current.Converters.ToArray();
-
-					// In umbraco, there are default value converters that try to convert the 
-					// value if all else fails. The problem is, they are also in the list of
-					// converters, and the means for filtering these out is internal, so
-					// we currently have to try ALL converters to see if they can convert
-					// rather than just finding the most appropreate. If the ability to filter
-					// out default value converters becomes public, the following logic could
-					// and probably should be changed.
-                    foreach (var converter in converters.Where(x => x.IsConverter(properyType)))
-                    {
-                        // Convert the type using a found value converter
-                        var value2 = converter.ConvertDataToSource(properyType, value, false);
-
-                        // If the value is of type T, just return it
-                        if (value2 is T)
-                            return (T)value2;
-
-                        // Value is not final value type, so try a regular type conversion aswell
-                        var convertAttempt = value2.TryConvertTo<T>();
-                        if (convertAttempt.Success)
-                            return convertAttempt.Result;
-
-                        // If ConvertDataToSource failed try ConvertSourceToObject.
-                        var value3 = converter.ConvertSourceToObject(properyType, value, false);
-
-                        // If the value is of type T, just return it
-                        if (value3 is T)
-                            return (T)value3;
-
-                        // Value is not final value type, so try a regular type conversion aswell
-                        var convertAttempt2 = value3.TryConvertTo<T>();
-                        if (convertAttempt2.Success)
-                            return convertAttempt2.Result;
-                    }
-
-                    // Value is not final value type, so try a regular type conversion
-                    var convertAttempt3 = value.TryConvertTo<T>();
-                    if (convertAttempt3.Success)
-                        return convertAttempt3.Result;
-
-					return default(T);
-				}
-				
-				if (prop.Value is T)
-				{
-					return (T)prop.Value;
-				}
-				
-				return default(T);
-			}
-
-			return defaultValue;
+		    return content.DoInnerGetVortoValue(propertyAlias, cultureName, recursive, defaultValue);
 		}
-
 
         public static T GetVortoValue<T>(this IPublishedContent content, string propertyAlias, string cultureName = null,
             bool recursive = false, T defaultValue = default(T), string fallbackCultureName = null)
         {
-            var result = content.DoGetVortoValue<T>(propertyAlias, cultureName, recursive);
+            var result = content.DoGetVortoValue<T>(propertyAlias, cultureName, recursive, default(T));
             if (result == null && !string.IsNullOrEmpty(fallbackCultureName) && !fallbackCultureName.Equals(cultureName))
                 result = content.DoGetVortoValue<T>(propertyAlias, fallbackCultureName, recursive, defaultValue);
 
             return result;
+        }
+
+        public static object GetVortoValue(this IPublishedContent content, string propertyAlias, string cultureName = null,
+            bool recursive = false, object defaultValue = null,
+            string fallbackCultureName = null)
+        {
+            return content.GetVortoValue<object>(propertyAlias, cultureName, recursive, defaultValue, fallbackCultureName);
         }
 
 	    #endregion
---
+++ Src/Our.Umbraco.Vorto/Extensions/VortoValueExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Our.Umbraco.Vorto.Models;
+
+namespace Our.Umbraco.Vorto.Extensions
+{
+    internal static class VortoValueExtensions
+    {
+        public static string FindBestMatchCulture(this VortoValue value, string cultureName)
+        {
+            // Check for exact match
+            if (value.Values.ContainsKey(cultureName))
+                return cultureName;
+
+            // Close match
+            return cultureName.Length == 2
+                ? value.Values.Keys.FirstOrDefault(x => x.StartsWith(cultureName + "-"))
+                : string.Empty;
+        }
+    }
+}

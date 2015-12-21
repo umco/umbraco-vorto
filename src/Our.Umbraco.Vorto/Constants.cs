4a052441d9016c20ba8bfc1922c754d283d512ae
mattbrailsford <me@mattbrailsford.com>
2015-12-21 09:47:01 +0000

    Force rebuild on appveyor

--- src/Our.Umbraco.Vorto/Constants.cs
+++ src/Our.Umbraco.Vorto/Constants.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Our.Umbraco.Vorto
 {
 	internal static class Constants
-    {
+    { 
 		public const string CacheKey_GetTargetDataTypeDefinition = "Vorto_GetTargetDataTypeDefinition_";
     }
 }

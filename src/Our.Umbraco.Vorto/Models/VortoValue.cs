b026f3f933c6a1668241c46dbe7b5a298f2773a3
ryanmcdonough <me@ryanmcdonough.co.uk>
2016-10-04 15:38:21 +0100

    Remove empty internal constructor
    As VortoValue is public, having the constructor as internal means that new VortoValue can't be created.

--- src/Our.Umbraco.Vorto/Models/VortoValue.cs
+++ src/Our.Umbraco.Vorto/Models/VortoValue.cs
@@ -6,9 +6,6 @@ namespace Our.Umbraco.Vorto.Models
 {
 	public class VortoValue
 	{
-	    internal VortoValue()
-	    { }
-
 		[JsonProperty("values")]
 		public IDictionary<string, object> Values { get; set; }
 
